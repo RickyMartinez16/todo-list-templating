10581d1ed1562d135b41f7036eb5782514ed1297
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-06 21:17:41 -0500

    local date string

--- app.js
+++ app.js
@@ -9,23 +9,16 @@ app.get("/", (req, res) => {
     
 
     let today = new Date();
-    let day = ""
 
-    if(today.getDay() === 0){
-        day = "Sunday"
-    } else if(today.getDay() === 1) {
-        day = "Monday"
-    } else if(today.getDay() === 2){
-        day = "Tuesday"
-    } else if(today.getDay() === 3){
-        day = "Wednesday"
-    } else if(today.getDay() === 4){
-        day = "Thursday"
-    } else if(today.getDay() === 5){
-        day = "Friday"
-    } else if(today.getDay() === 6){
-        day = "Saturday"
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+
+    let day = today.toLocaleDateString("en-US" , options)
+
+   
     res.render("list", {kindOfDay: day})
 })
 
