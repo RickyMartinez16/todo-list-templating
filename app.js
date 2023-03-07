e311a1da3f625ad877eef6db13a6819d109f0b9e
Ricky Martinez <RMartinezW16@gmail.com>
2023-03-06 21:10:56 -0500

    getDay

--- app.js
+++ app.js
@@ -3,16 +3,30 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
     
 
-    let day = new Date();
+    let today = new Date();
+    let day = ""
 
-    if(day.getDate() === 6 || day.getDate() === 0){
-        res.send("yay its the weekend")
-    } else {
-        res.send("boo it's a workday")
+    if(today.getDay() === 0){
+        day = "Sunday"
+    } else if(today.getDay() === 1) {
+        day = "Monday"
+    } else if(today.getDay() === 2){
+        day = "Tuesday"
+    } else if(today.getDay() === 3){
+        day = "Wednesday"
+    } else if(today.getDay() === 4){
+        day = "Thursday"
+    } else if(today.getDay() === 5){
+        day = "Friday"
+    } else if(today.getDay() === 6){
+        day = "Saturday"
     }
+    res.render("list", {kindOfDay: day})
 })
 
 app.listen(3000, () => {
